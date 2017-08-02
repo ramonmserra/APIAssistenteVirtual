01c518ffeca2b547d0625ba4fd17c26833eda59e
Ramon Vitor Malaquias Serra <ramon.serra@alctel.com.br>
2017-08-02 13:12:55 -0300

    api

---
+++ WebAPI/WebAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Newtonsoft.Json;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+
+    [RoutePrefix("api/produto")]
+    public class UsuarioController : ApiController
+    {
+        private static List<UsuarioModel> listaUsuarios = new List<UsuarioModel>();
+
+        [AcceptVerbs("POST")]
+        [Route("CadastrarProduto")]
+        public string CadastrarProduto(UsuarioModel usuario)
+        {
+
+            listaUsuarios.Add(usuario);
+
+            return "Produto cadastrado com sucesso!";
+        }
+
+       /* [AcceptVerbs("PUT")]
+        [Route("AlterarProduto")]
+        public string AlterarProduto(UsuarioModel usuario)
+        {
+
+            listaUsuarios.Where(n => n.CodProduto == usuario.CodProduto)
+                         .Select(s =>
+                         {
+                             s.CodProduto = usuario.CodProduto;
+                             s.Login = usuario.Login;
+                             s.Nome = usuario.Nome;
+
+                             return s;
+
+                         }).ToList();
+
+
+
+            return "Usuário alterado com sucesso!";
+        }
+        */
+        [AcceptVerbs("DELETE")]
+        [Route("ExcluirUsuario/{codproduto}")]
+        public string ExcluirUsuario(int codproduto)
+        {
+
+            UsuarioModel usuario = listaUsuarios.Where(n => n.CodProduto == codproduto)
+                                                .Select(n => n)
+                                                .First();
+
+            listaUsuarios.Remove(usuario);
+
+            return "Registro excluido com sucesso!";
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("ConsultarPrecoPorCodigo/{codproduto}")]
+        public float ConsultarPrecoPorCodigo(int codproduto)
+        {
+
+            UsuarioModel usuario = listaUsuarios.Where(n => n.CodProduto == codproduto)
+                                                .Select(n => n)
+                                                .FirstOrDefault();
+
+    
+            return usuario.Preco;
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("ConsultarProdutos")]
+        public List<UsuarioModel> ConsultarProdutos()
+        {
+            return listaUsuarios;
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Models/UsuarioModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class UsuarioModel
+    {
+
+        private int codproduto;
+        private string produto;
+        private float preco;
+        private int status;
+  
+
+        public UsuarioModel()
+        {
+        }
+
+        public UsuarioModel(int codproduto, string produto, float preco , int status)
+        {
+            this.CodProduto = codproduto;
+            this.Produto = produto;
+            this.Preco = preco;
+            this.Status = status;
+        }
+
+        public int CodProduto        {
+            get
+            {
+                return codproduto;
+            }
+
+            set
+            {
+                codproduto = value;
+            }
+        }
+
+        public string Produto
+        {
+            get
+            {
+                return produto;
+            }
+
+            set
+            {
+                produto = value;
+            }
+        }
+
+        public float Preco
+        {
+            get
+            {
+                return preco;
+            }
+
+            set
+            {
+                preco = value;
+            }
+        }
+
+        public int Status
+        {
+            get
+            {
+                return status;
+            }
+
+            set
+            {
+                status = value;
+            }
+        }
+
+    }
+}
